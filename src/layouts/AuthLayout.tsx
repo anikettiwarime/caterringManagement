5706c0e9be7ba7cba636df4635e364efdfad9f2a
Aniket Tiwari <anikettiwari1570@gmail.com>
2024-08-08 23:52:22 +0530

    refactor: Remove unused logo images and update image import in AuthLayout

--- src/layouts/AuthLayout.tsx
+++ src/layouts/AuthLayout.tsx
@@ -1,7 +1,8 @@
 import {useAuthContext} from '@/context/AuthContext';
-import {Link, Navigate, Outlet} from '@tanstack/react-router';
-import MenuImage1 from '../assets/images/logo/MenuImage1.jpg';
-import {AuthIcon} from '@/components/Icons';
+import {Navigate, Outlet} from '@tanstack/react-router';
+// import MenuImage1 from '../assets/images/logo/MenuImage1.jpg';
+import MenuBook from '../assets/images/logo/Menubook.png';
+// import {AuthIcon} from '@/components/Icons';
 
 const AuthLayout = () => {
   const {isAuthenticated} = useAuthContext();
@@ -15,10 +16,12 @@ const AuthLayout = () => {
       <div className="flex flex-wrap items-center">
         <div className="hidden w-full xl:block xl:w-1/2">
           <div className="px-26 py-17.5 text-center">
-            <Link className="mb-5.5 inline-block" to="/">
+            {/* <Link className="mb-5.5 inline-block" to="/">
               <img className="w-50 dark:block" src={MenuImage1} alt="Logo" />
-            </Link>
-            <AuthIcon />
+            </Link> */}
+            {/* <AuthIcon /> */}
+
+            <img className="w-full dark:hidden" src={MenuBook} alt="Logo" />
           </div>
         </div>
         <div className="w-full border-stroke dark:border-strokedark xl:w-1/2 xl:border-l-2">
